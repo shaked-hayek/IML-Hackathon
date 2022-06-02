ec4d08110964cb7c1eb56e8e6c9f9759ef4d00f7
Guy Kaplan <guy.kaplan@cs.huji.ac.il>
2022-06-02 22:12:44 +0300

    part 2 predicts using adaboost (no loss check)

--- tumor_size_estimator.py
+++ tumor_size_estimator.py
@@ -1,21 +1,27 @@
 from os import path
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
-from preprocessing import load_data, DATA_PATH, TRAIN_FILE, LABELS_FILE_2
+from sklearn.ensemble import RandomForestRegressor, \
+    AdaBoostRegressor
+from preprocessing import load_data_question_2, DATA_PATH, TRAIN_FILE, LABELS_FILE_2
 
 LABEL_TITLE = "Tumor Size"  # TODO check the right label header
 PRED_FILE = "prediction.csv"  # TODO make sure it doesn't overrun the other part's predictions!
 # TODO also address the gold part...
 
+def estimate_tumor_size(train_X, train_y, test_X):
+    ada = AdaBoostRegressor()
+    ada.fit(train_X, train_y)
+    pred = ada.predict(test_X)
+    return pred
+
 
 def main():
-    train_X, train_y = load_data(path.join(DATA_PATH, TRAIN_FILE))
-    part2_model = RandomForestRegressor()  # only temporary #TODO choose the right one
-    part2_model.fit(train_X, train_y)
-    test_X, _ = load_data(path.join(DATA_PATH, LABELS_FILE_2))
-    pd.DataFrame(part2_model.predict(test_X), columns=[LABEL_TITLE]).to_csv(PRED_FILE, index=False)
+    X, y = load_data_question_2(path.join(DATA_PATH, TRAIN_FILE), path.join(DATA_PATH, LABELS_FILE_2))
+    test_X, _ = load_data_question_2(path.join(DATA_PATH, "test.feats.csv"))
+    predictions = estimate_tumor_size(X, y, test_X)
 
+    pd.DataFrame(predictions).to_csv(PRED_FILE)
 
 if __name__ == "__main__":
     main()
