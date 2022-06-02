cc918450eddf222e17eef02bdbda325418ebc1c7
unknown <shaked.hayek@mail.huji.ac.il>
2022-06-02 13:50:07 +0300

    surgery parse

--- preprocessing.py
+++ preprocessing.py
@@ -10,11 +10,27 @@ TRAIN_FILE = "train.feats.csv"
 
 DATES_COLS = ["אבחנה-Diagnosis date", "אבחנה-Surgery date1",
               "אבחנה-Surgery date2", "אבחנה-Surgery date3"]
+SUR_SUM_COL = "אבחנה-Surgery sum"
+SUR_DATES_COL = ["אבחנה-Surgery date1", "אבחנה-Surgery date2", "אבחנה-Surgery date3"]
+SUR_SINCE_LAST = "Time since last surgery"
+
+
+def surgery_process(df):
+    # Surgery sum
+    df[SUR_SUM_COL].replace({None: "0"}, inplace=True)
+
+    # Dates
+    # for col in SUR_DATES_COL:
+    #     df[col] = pd.to_datetime(df[col])
+    #     #df[col].replace({None: "0"}, inplace=True)
+    # df[SUR_SINCE_LAST] = df[SUR_DATES_COL].max(axis=1)
+    # print(df[SUR_SINCE_LAST])
 
 
 def load_data(file_path):
-    df = pd.read_csv(file_path, parse_dates=DATES_COLS, dayfirst=True)
-    # parse?
+    df = pd.read_csv(file_path, dtype='unicode', parse_dates=DATES_COLS, dayfirst=True)
+    surgery_process(df)
+
     return df
 
 
