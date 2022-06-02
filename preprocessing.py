1a83f84571a409e0f5698288387945c1a2e79ba5
Niv <niv.eckhaus@gmail.com>
2022-06-02 13:52:45 +0300

    working on er_pr_preprocess

--- preprocessing.py
+++ preprocessing.py
@@ -12,8 +12,19 @@ DATES_COLS = ["אבחנה-Diagnosis date", "אבחנה-Surgery date1",
               "אבחנה-Surgery date2", "אבחנה-Surgery date3"]
 
 
+def er_pr_preprocess(cell_data):
+    """
+    # TODO document
+    """
+
+
+
 def load_data(file_path):
     df = pd.read_csv(file_path, parse_dates=DATES_COLS, dayfirst=True)
+
+    df["אבחנה-er"] = df["אבחנה-er"].astype(str).apply(er_pr_preprocess)
+    df["אבחנה-pr"] = df["אבחנה-pr"].astype(str).apply(er_pr_preprocess)
+
     # parse?
     return df
 
